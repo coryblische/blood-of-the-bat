aec616328361f4b37f080a0eba1375ef5a0c7021
cory <cblische@oxfordclub.com>
2025-09-07 14:14:42 -0400

    refactor: Clean up comments and update Open Graph image path in Meta component

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -3,23 +3,12 @@ import Head from 'next/head'
 const Meta = () => {
   return (
     <Head>
-      {/* SVG Favicon for modern browsers */}
       <link rel='icon' type='image/svg+xml' href='/favicon/favicon.svg' />
-
-      {/* PNG Favicons for desktop */}
       <link rel='icon' type='image/png' href='/favicon/icon1.png' />
-
-      {/* ICO Favicon for legacy browsers */}
       <link rel='shortcut icon' href='/favicon/favicon.ico' />
-
-      {/* Apple Touch Icon */}
       <link rel='apple-touch-icon' href='/favicon/apple-icon.png' />
-
-      {/* Web App Meta */}
       <meta name='apple-mobile-web-app-title' content='Blood of the Bat' />
       <meta name='application-name' content='Blood of the Bat' />
-
-      {/* Manifest and other icons */}
       <link rel='manifest' href='/favicon/manifest.json' />
       <link
         rel='mask-icon'
@@ -34,7 +23,10 @@ const Meta = () => {
         name='description'
         content='Blood of the Bat is a blog dedicated to all things Black Sabbath, featuring our tribute band news, gig recaps, song breakdowns, and more.'
       />
-      <meta property='og:image' content='/favicon/safari-pinned-tab.svg' />
+      <meta
+        property='og:image'
+        content='/public/images/chatgpt-image-sep-6--2025--11_41_44-am-QxND.png'
+      />
     </Head>
   )
 }
